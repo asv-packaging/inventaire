2232be23deaa1ec0d3beced39f65820e45cd228e
Steven <dupont.steven36@proton.me>
2024-03-19 16:17:21 +0100

    update: fix errors deprecated routing

--- src/Controller/EcranController.php
+++ src/Controller/EcranController.php
@@ -22,7 +22,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 
